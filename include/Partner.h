c7e0920decb63b1a621d3715c0f02a55a64ad9fc
david51db <davidgabrielpopa@gmail.com>
2026-04-19 17:35:23 +0300

    -added partner class with constructors and operators

---
+++ include/Partner.h
@@ -0,0 +1,30 @@
+//
+// Created by David on 4/19/2026.
+//
+
+#ifndef GETREADYFORYOURDATE_PARTNER_H
+#define GETREADYFORYOURDATE_PARTNER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Partner {
+    std::string name;
+    std::vector<std::string> traits;
+    std::string secretTrait;
+
+public:
+    Partner();
+    Partner(std::string name, std::vector<std::string>& traits, std::string secretTrait);
+    Partner(const Partner& obj);
+    Partner& operator=(const Partner& obj);
+    ~Partner();
+
+    friend std::ostream& operator<<(std::ostream& os, const Partner& obj);
+    friend std::istream& operator>>(std::istream& is, Partner& obj);
+};
+
+
+
+#endif //GETREADYFORYOURDATE_PARTNER_H
---
+++ src/Partner.cpp
@@ -0,0 +1,69 @@
+//
+// Created by David on 4/19/2026.
+//
+
+#include "Partner.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+Partner::Partner() {
+    name="N/A";
+    traits.push_back("N/A");
+    secretTrait="N/A";
+}
+
+Partner::Partner(string name, vector<string>& traits, string secretTrait) {
+    this->name=name;
+    this->traits=traits;
+    this->secretTrait=secretTrait;
+}
+
+Partner::Partner(const Partner &obj) {
+    this->name=obj.name;
+    this->traits=obj.traits;
+    this->secretTrait=obj.secretTrait;
+}
+
+Partner& Partner::operator=(const Partner &obj) {
+    if (this==&obj) return *this;
+
+    this->name=obj.name;
+    this->traits=obj.traits;
+    this->secretTrait=obj.secretTrait;
+
+    return *this;
+}
+
+ostream& operator<<(ostream &os, const Partner &obj) {
+    os<<"Name: " << obj.name<< "\n";
+
+    os<<"Traits: " ;
+    for (const string& trait: obj.traits) os<<trait<<" ";
+    os<<"\n";
+
+    os<<"Secret Trait: "<<obj.secretTrait<<"\n";
+
+    return os;
+}
+
+istream& operator>>(istream &is, Partner& obj) {
+
+    cout<<"Name: ";
+    is>>obj.name;
+
+    int traitsNumber;
+    string trait;
+    cout<<"Number of traits: ";
+    is>>traitsNumber;
+    for (int i=0; i<traitsNumber;i++) {
+        cout<<"Trait "<<i+1<<": ";
+        is>>trait;
+        obj.traits.push_back(trait);
+    }
+
+    cout<<"Secret Trait: ";
+    is>>obj.secretTrait;
+
+    return is;
+}
